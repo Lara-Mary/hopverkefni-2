8fe9059346df9e6cded3f65f83dc4bec1a0921b5
Lara-Mary <lml5@hi.is>
2024-11-20 20:32:04 +0000

    tók út ónotað

--- src/lib/components/keywords.js
+++ src/lib/components/keywords.js
@@ -3,22 +3,6 @@ import { el } from '../elements.js';
 export function renderKeywords(indexJson, contentJson) {
   console.log('rendering keywords page');
 
-  const mainElement = el(
-    'main',
-    {},
-    el(
-      'section',
-      {}, 
-      el('p', {}, indexJson.description),
-      el(
-        'p',
-        {},
-        'Ég er content page, þetta er contentið mitt' +
-          JSON.stringify(contentJson),
-      ),
-    ),
-  );
-
   const wrapperElement = el('main', {});
   const titleElement = el('h2', {}, contentJson.title);
   wrapperElement.appendChild(titleElement);
