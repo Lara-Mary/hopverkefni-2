2d23c1cc83f231f1d1bf457879cb2bd44bef4781
Gunnar Ingi <gunnaringi@Gunnars-MacBook-Air-2.local>
2024-11-22 11:46:05 +0000

    Bæti við öllum breytingum í gunnar_branch

---
+++ src/lib/pages/index-page.js
@@ -0,0 +1,25 @@
+import { renderNavigation } from '../components/navigation.js';
+import { el } from '../elements.js';
+
+export function renderIndexPage(root, indexJson) {
+  console.log('rendering', root, indexJson.title);
+
+  const headerElement = el('header', {}, el('h1', {}, indexJson.title));
+
+  headerElement.appendChild(renderNavigation(indexJson.navigation));
+
+  const mainElement = el(
+    'main',
+    {},
+    el(
+      'section',
+      {},
+      el('p', {}, indexJson.description),
+    ),
+  );
+  const footerElement = el('footer', {}, indexJson.footer);
+
+  root.appendChild(headerElement);
+  root.appendChild(mainElement);
+  root.appendChild(footerElement);
+}
