402f5beebd202fed7929e49a1588d667ea4c105f
Gunnar Ingi <gunnaringi@Gunnars-MacBook-Air-2.local>
2024-11-22 11:28:40 +0000

    Bæti við sub-page.js, content-page.js, lectures.js og styles.css

---
+++ src/lib/pages/content-page.js
@@ -0,0 +1,46 @@
+import { renderKeywords } from '../components/keywords.js';
+import { renderNavigation } from '../components/navigation.js';
+import { el } from '../elements.js';
+import { fetcher } from '../fetcher.js';
+import { renderLectures } from '../components/lectures.js'; 
+
+export async function renderContentPage(root, indexJson, type, content) {
+  console.log('rendering content page', root, indexJson.title);
+
+  const contentJson = await fetcher(`data/${type}/${content}.json`);
+
+  const headerElement = el('header', {}, el('h1', {}, indexJson.title));
+
+  headerElement.appendChild(renderNavigation(indexJson.navigation));
+
+  let mainElement;
+
+  if(content === 'keywords') {
+    mainElement = renderKeywords(indexJson, contentJson);
+  } else if (content === 'lectures') {
+    mainElement = renderLectures(indexJson, contentJson); 
+  }
+  else {
+    mainElement = el(
+      'main',
+      {},
+      el(
+        'section',
+        {}, 
+        el('p', {}, indexJson.description),
+        el(
+          'p',
+          {},
+          'Ég er content page, þetta er contentið mitt' +
+            JSON.stringify(contentJson),
+        ),
+      ),
+    );
+  }
+
+  const footerElement = el('footer', {}, indexJson.footer);
+
+  root.appendChild(headerElement);
+  root.appendChild(mainElement);
+  root.appendChild(footerElement);
+}
---
+++ src/lib/pages/sub-page.js
@@ -0,0 +1,55 @@
+import { renderNavigation } from '../components/navigation.js';
+import { el } from '../elements.js';
+import { fetcher } from '../fetcher.js';
+
+export async function renderSubpage(root, indexJson, type) {
+  const headerElement = el('header', {}, el('h1', {}, indexJson.title));
+
+  headerElement.appendChild(renderNavigation(indexJson.navigation));
+
+  let foundType = null;
+
+  if (indexJson.navigation.find((i) => i.slug === type)) {
+    foundType = type;
+  }
+
+  let mainElement;
+  if (!foundType) {
+    mainElement = el('main', {}, el('p', {}, 'Fannst ekki'));
+  } else {
+    const contentJsonFile = `data/${type}/index.json`;
+    const contentJson = await fetcher(contentJsonFile);
+
+    const content = contentJson.content;
+    const contentElement = el('div', {class: 'contentWrapper'})
+
+    // TODO ættum að skoða html structure hér
+    for (const item of content) {
+      const itemElement = document.createElement('section');
+      
+      const href = window.location.search + `&content=${item.slug}`;
+      console.log('content href', href);
+      
+      const button = document.createElement('button');
+      const link = el('a', { href }, item.title);
+      button.appendChild(link);
+      itemElement.appendChild(button);
+
+      const itemText = document.createElement('div');
+      itemText.textContent = item.text;
+
+      itemElement.appendChild(button);
+      itemElement.appendChild(itemText);
+
+      contentElement.appendChild(itemElement);
+    }
+
+    mainElement = el('main', {}, contentElement);
+  }
+
+  const footerElement = el('footer', {}, indexJson.footer);
+
+  root.appendChild(headerElement);
+  root.appendChild(mainElement);
+  root.appendChild(footerElement);
+}
